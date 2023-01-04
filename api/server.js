7b5fcda8ffc395bac5dec060f5a0967f43f6f40f
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-04 16:40:35 -0700

    implement server and endpoints

--- api/server.js
+++ api/server.js
@@ -1,2 +1,13 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const postsRoutes = require('./posts/posts-router');
+
+
+server.use('/posts', postsRoutes)
+module.exports = server
