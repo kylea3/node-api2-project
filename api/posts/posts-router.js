f401c79c75f71007e4d9b97f5095aa8c71f1426c
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-04 17:19:35 -0700

    create get response in posts-router.js

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -14,7 +14,20 @@ router.get('/', (req, res) => {
         }) 
 })
 
-
+router.get('/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id); 
+    if(!post) {
+        res.status(404).json({ message: "The post with the specified ID does not exist" })
+    } else {
+        Post.findById(req.params.id)
+            .then(post => {
+                res.status(200).json(post)
+            })
+            .catch(err => {
+                res.status(500).json({ message: "The post information could not be retrieved" })
+            })
+    }
+})
 
 
 module.exports = router;
