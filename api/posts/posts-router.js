d46552816012a316f9bf5486f71039824688427e
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-05 09:46:28 -0700

    add get response for comments to posts-router.js

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -87,6 +87,20 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
+router.get('/:id/comments', async (req, res) => {
+    const comments = await Post.findById(req.params.id);
+    if(!comments) {
+        res.status(404).json({ message: "The post with the specified ID does not exist" })
+    } else {
+        Post.findPostComments(req.params.id)
+            .then(comment => {
+                res.status(200).json(comment)
+            })
+            .catch(err => {
+                res.status(500).json({ message: "The comments information could not be retrieved" })
+            })
+    }
+})
 
 module.exports = router;
 
